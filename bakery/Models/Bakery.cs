8f921aeedb44c7acac93baed5af151573d82f7a4
Grace Kostanich <fl5812597@gmail.com>
2022-07-15 16:55:35 -0700

    Add entire Pastry class holding necessary methods, loops, and if statements (identical to bread class)

--- bakery/Models/Bakery.cs
+++ bakery/Models/Bakery.cs
@@ -42,4 +42,45 @@ namespace Bakery.Models
       return breadTotal;
     }
   }
+
+    public class Pastry
+  { 
+    private static double _Price = 2;
+    private double _Deal;
+    public int Quantity { get; set; } 
+
+    public static double Price 
+    {
+      get { return _Price; }
+      set { _Price = value; }
+    }
+
+    public Pastry(int quantity)
+    {
+      _Price = Price;
+      _Deal = 0.5;
+      Quantity = quantity;
+    }
+
+    public double PastryCost()
+    {
+      double pastryTotal = 0;
+      for (int index = 0; index <= Quantity; index++)
+      {
+        if (index == 0)
+        {
+          pastryTotal += 0;
+        }
+        else if (index % 3 == 0)
+        {
+          pastryTotal += _Price * _Deal;
+        }
+        else
+        {
+          pastryTotal += _Price;
+        }
+      }
+      return pastryTotal;
+    }
+  }
 }  
