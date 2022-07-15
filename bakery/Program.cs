2553920f7a35ecffd1bef33413f210bce91efd12
Grace Kostanich <fl5812597@gmail.com>
2022-07-15 16:57:31 -0700

    Add UI for pastry input/output

--- bakery/Program.cs
+++ bakery/Program.cs
@@ -2,14 +2,13 @@ using System;
 using System.Collections.Generic;
 using Bakery.Models;
 
-//This stuff is basically UI logic
 namespace Bakery.Models
 {
   public class Program
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery! Here's our Menu: Bread - $" + Bread.Price);
+      Console.WriteLine("Welcome to Pierre's Bakery! Here's our Menu: Bread - $" + Bread.Price + " | Pastries - $" + Pastry.Price);
       Console.WriteLine("Would you like to buy any bread today? (Type 'y' or 'n')");
       string response = Console.ReadLine();
         if (response == "y")
@@ -19,6 +18,21 @@ namespace Bakery.Models
             Bread usersBreadOrder = new Bread(breadOrder);
             Console.WriteLine("Your Order " + usersBreadOrder.Quantity + " loaf/loaves"); 
             Console.WriteLine("Cost: $" + usersBreadOrder.BreadCost());
+        }
+        else 
+        {
+            Console.WriteLine("Thanks user, ");
+        }
+
+            Console.WriteLine("would you like to buy any pastries today? (Type 'y' or 'n')");
+            string response2 = Console.ReadLine();
+        if (response2 == "y")
+        {
+            Console.WriteLine("How many would you like? (Enter a Number)");
+            int pastryOrder = int.Parse(Console.ReadLine());
+            Pastry usersPastryOrder = new Pastry(pastryOrder);
+            Console.WriteLine("Your Order: " + usersPastryOrder.Quantity + " pastries"); 
+            Console.WriteLine("Cost: $" + usersPastryOrder.PastryCost());
         }
         else 
         {
