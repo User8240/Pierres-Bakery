fa243134bc2adbbc2f43f760d8b2523add02f418
Grace Kostanich <fl5812597@gmail.com>
2022-07-15 14:51:02 -0700

    Create if else statement for Bread

--- bakery/Program.cs
+++ bakery/Program.cs
@@ -1,30 +1,29 @@
 using System;
 using System.Collections.Generic;
 using Bakery.Models;
+
 //This stuff is basically UI logic
-namespace Bakery
+namespace Bakery.Models
 {
   public class Program
   {
     public static void Main()
     {
-      //create bread and pastries instances of their provided class
-      Bread bread = new Bread("Bread", 0);
-      List<Bread> BreadsList = new List<Bread>() { bread };
-
-      Pastries pastries = new Pastries("Pastry", 0)
-      List<Pastries> PastriesList = new List<Pastries>() { pastries };
-
-      Console.WriteLine("Welcome to Pierre's Bakery! Here's our Menu: | Bread - $ | Pastries - $ |");
-      Console.WriteLine("Would you like to buy any bread today? (type 'y' or 'n'");
-      //Take user input for bread (Y or N) and store in breadYoN variable
-      string breadYoN = Console.ReadLine();
-      
-      List<Bread> UserInputBread = new List<bread>(0);
-
-      foreach (Bread element in ) 
-      if 
-
+      Console.WriteLine("Welcome to Pierre's Bakery! Here's our Menu: Bread - $" + Bread.Price);
+      Console.WriteLine("Would you like to buy any bread today? (Type 'y' or 'n')");
+      string response = Console.ReadLine();
+        if (response == "y")
+        {
+            Console.WriteLine("How many loaves would you like? (Enter a Number)");
+            int breadOrder = int.Parse(Console.ReadLine());
+            Bread usersBreadOrder = new Bread(breadOrder);
+            Console.WriteLine("Your Order " + usersBreadOrder.Quantity + " loaf/loaves"); 
+            Console.WriteLine("Cost: $" + usersBreadOrder.BreadCost());
+        }
+        else 
+        {
+            Console.WriteLine("Thanks user, Come again!");
+        }
     }
   }
 }
