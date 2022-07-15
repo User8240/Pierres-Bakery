9bb4b91fb11bd97441f3d934690b3884e0f0ca75
Grace Kostanich <fl5812597@gmail.com>
2022-07-15 13:53:31 -0700

    Completely refactor code | Restart

--- bakery/Models/Bakery.cs
+++ bakery/Models/Bakery.cs
@@ -3,62 +3,43 @@ using System;
 namespace Bakery.Models
 {
   public class Bread
-  { //Create & make variables private
-    private string _bread;
-    private int _price;
+  { 
+    private static int _Price = 5;
+    private int _Deal;
+    public int Quantity { get; set; } 
 
-    public Bread(string bread, int price)
+    public static int Price 
     {
-      _bread = bread;
-      _price = price;
+      get { return _Price; }
+      set { _Price = value; }
     }
 
-    //Get-ting the private variables
-    public string GetBread()
+    public Bread(int quantity)
     {
-      return _bread;
+      _Price = Price;
+      _Deal = 0.5;
+      Quantity = quantity;
     }
 
-    public int GetPrice()
+    public int BreadCost()
     {
-      return _price;
-    }
-
-    //Method for bread selection
-
-    public bool SelectBread(string selectBread)
-    {
-    return (_bread == selectBread);
-    }
-  }
-//------------------------------------------------------------------
-    public class Pastries
-  { //Create & make variables private
-    private string _pastries;
-    private int _price;
-
-    public Pastries(string pastries, int price)
-    {
-      _pastries = pastries;
-      _price = price;
-    }
-
-    //Get-ting the private variables
-    public string GetPastries()
-    {
-      return _pastries;
-    }
-
-    public int GetPrice()
-    {
-      return _price;
-    }
-
-    //Method for Pastry selection
-
-    public bool SelectPastry(string selectPastry)
-    {
-      return (_pastries == selectPastry);
+      double breadTotal = 0;
+      for (int index = 0; index <= Quantity; index++)
+      {
+        if (index == 0)
+        {
+          breadTotal += 0;
+        }
+        else if (index % 3 == 0)
+        {
+          breadTotal += _Price * _Deal;
+        }
+        else
+        {
+          breadTotal += _Price;
+        }
+        return breadTotal;
+      }
     }
   }
-}
+}  
