23804b007395c7a9c9d4fdc39f6d0987bc8c391c
Grace Kostanich <fl5812597@gmail.com>
2022-07-15 14:53:12 -0700

    Add passing test | 1 order of bread returns base bread price

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -1,16 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using Bakery.Models;
 
 namespace Bakery.Test
 {
   [TestClass]
-  public class BakeryTest
+  public class BreadTests
   {
     [TestMethod]
-    public void MethodName_WhatItDo_ToF()
+    public void BreadCost_CostOneLoaf_Int()
     {
-      Class newInstance = new Class ("pass in something");
-      Assert.AreEqual(ToF, newInstance.MethodName());
+      //Arrange 
+      int breadQuantity = 1;
+      double breadCost = 5;
+      //Act
+      Bread breadOrder = new Bread(breadQuantity);
+      //Assert
+      Assert.AreEqual(breadCost, breadOrder.BreadCost());
     }
   }
 }
